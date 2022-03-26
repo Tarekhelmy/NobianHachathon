0bd9b3454187c4c0a3911dbdc23722c3a4633f71
bubblydiddles <38568275+bubblydiddles@users.noreply.github.com>
2022-03-26 17:01:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-End=[805440,846848]
 
 Names=['valve (A) timestamp', 'valve (A) output value',
        'flow setpoint (B) timestamp', 'flow setpoint (B) value',
@@ -18,54 +13,71 @@ def ExcelPickler():
     csvreader.to_pickle('cached_dataframe.pkl')
     return None
 
-def Removebad():
-    data = pd.DataFrame(pd.read_pickle('cached_dataframe.pkl'))
-    indexs= []
-    A, TimeA = data['valve (A) output value'], data['valve (A) timestamp']
-    C, TimeC = data['flow measurment (C) value'], data['flow measurment (C) timestamp']
-    B, TimeB = data['flow setpoint (B) value'], data['flow setpoint (B) timestamp']
-    for i in range(len(A)):
-        if A[i]=="Bad":
-            indexs.append(i)
-    for i in range(len(C)):
-        if C[i]=="Bad":
-            indexs.append(i)
-    for i in range(len(B)):
-        if B[i]=="Bad":
-            indexs.append(i)
-    data=data.drop(indexs)
-    data.to_pickle('cached_dataframe_edited.pkl')
-
-
-def Plot():
-    data=pd.read_pickle('cached_dataframe_edited.pkl')
+def Plot(data):
     plt.plot(data['valve (A) timestamp'][0:10000],data['valve (A) output value'][0:10000],label="A")
     plt.plot(data['flow measurment (C) timestamp'][0:10000],data['flow measurment (C) value'][0:10000],label="C")
     plt.legend()
     plt.show()
     print(data.keys())
 
-def ROC(Start,End):
-    data = pd.read_pickle('cached_dataframe_edited.pkl')
-    A ,TimeA =  data['valve (A) output value'][Start:End[0]] ,data['valve (A) timestamp'][Start:End[0]]
-    C ,TimeC =  data['flow measurment (C) timestamp'][Start:End[-1]] ,data['flow measurment (C) value'][Start:End[-1]]
+# Result of Last Indices[805440, 644258, 846848]
+
+def find_last_indices(data):
+    last_index = []
+    last_index.append(data['valve (A) timestamp'].last_valid_index())
+    last_index.append(data['flow setpoint (B) timestamp'].last_valid_index())
+    last_index.append(data['flow measurment (C) timestamp'].last_valid_index())
+    return last_index
+
+def drop_bad(data):
+    new_data = data.drop(data[data['valve (A) output value'] == 'Bad'].index)
+    new_data = new_data.drop(data[data['flow setpoint (B) value'] == 'Bad'].index)
+    new_data = new_data.drop(data[data['flow measurment (C) value'] == 'Bad'].index)
+    return new_data
+
+def ROC(data,Start,End):
+    A, TimeA =  data['valve (A) output value'][Start:End[0]],data['valve (A) timestamp'][Start:End[0]]
+    C, TimeC =  data['flow measurment (C) timestamp'][Start:End[2]] ,data['flow measurment (C) value'][Start:End[2]]
     A = A.to_numpy()
     A = np.asarray(A)
-    C = C.to_numpy()
-    C = np.asarray(A)
     Errors = 0
     Adiff = np.diff(A)
     Cdiff = np.diff(C)
     Aedit = A[::5]
-    Cedit = C[::5]
     for i in range(len(Aedit)):
         if A[i]<5:
             Errors+=1
     print(Errors)
     return None
 
-
+# def clog_finder(data,start,end):
+#
+#     return
 if __name__ == '__main__':
-    # ROC(0,End)
-    # Removebad()
-    Plot()
+    data = pd.read_pickle('cached_dataframe.pkl')
+    clean_data = drop_bad(data)
+    end_indices = find_last_indices(clean_data)
+    # ROC(clean_data, 0, end_indices)
+    # Plot(clean_data)
+    Start = 0
+    End = 10000
+    A, TimeA = clean_data['valve (A) output value'][Start:End], clean_data['valve (A) timestamp'][Start:End]
+    C, TimeC = clean_data['flow measurment (C) value'][Start:End], clean_data['flow measurment (C) timestamp'][Start:End]
+    TimeA = np.int64(TimeA)
+    t0 = TimeA[0]
+    TimeA = (TimeA - t0)/(10**9)
+
+    TimeC = np.int64(TimeC)
+    TimeC = (TimeC - t0)/(10**9)
+
+    t_span = np.linspace(TimeA[0], TimeA[-1], num=len(TimeC))
+    A = np.array(A, dtype=np.float64)
+    C = np.array(C, dtype=np.float64)
+    A = np.interp(t_span, TimeA, A)
+    C = np.interp(t_span, TimeC, C)
+
+    difference = A-C
+    plt.plot(t_span[0:-1], np.diff(difference))
+    plt.plot(t_span[0:-1], A[0:-1], label = 'A')
+    plt.plot(t_span[0:-1], C[0:-1], label = 'C')
+    plt.show()
